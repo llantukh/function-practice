3ed6329c684599f125a8a12452ea92d4a25c1ceb
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-06 09:21:53 -0400

    not finished, but due

--- excercises-1.js
+++ excercises-1.js
@@ -23,21 +23,27 @@
 // Q1
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
-
+var sum = function(x,y){
+  return  x + y;
+}
 
 
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
-
+var avg = function(x,y,z){
+  return (x + y + z) / 3;
+}
 
 
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
-
+var getLength = function("pineapples"){
+  
+}
 
 
 
@@ -46,8 +52,13 @@
 // two parameters and returns `true` if the second
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
-
-
+var greaterThan = function (x,y){
+  if (x > y) {
+    return "true";
+  } else {
+    return "false";
+  }
+};
 
 
 // Q5
@@ -55,7 +66,9 @@
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
+var greet = function (Lela) {
 
+}
 
 
 
